c85657dfb3e2b02d7ce8388f58b475ea85e0c024
muxik <lqjxm666@163.com>
2020-10-15 14:58:18 +0800

    修复部分bug,使用多线程进行异步下载

--- meizi.py
+++ meizi.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Author: Muxi_K
-
 import requests
 import re
-from datetime import datetime
-import time
 import os
+import time
+import sys
+from tqdm import tqdm
+from multiprocessing import Pool
+
 
 class Mzitu:
+    
+    
     # Http Headers
     headers = {
         'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
@@ -16,6 +17,7 @@ class Mzitu:
         'Referer': "http://www.mzitu.com/"
     }
 
+    
     # urls
     urls = [
         # 性感妹子
@@ -26,10 +28,30 @@ class Mzitu:
         "https://www.mzitu.com/taiwan/",
         # 清纯妹子
         "https://www.mzitu.com/mm/",
+        # 全部妹子
+        "https://www.mzitu.com/",
     ]
+    
+    dirs = [
+            # 性感妹子
+            "xingan/",
+            # 日本
+            "japan/",
+            # 台湾
+            "taiwan/",
+            # 清纯
+            "mm/",
+            # 全部
+            "all/"
+            ]
 
-    # 初始化
+    path = 'data/'
+
+
     def __init__(self):
+
+        os.system("clear") 
+
         output = \
         """
          __  __ ________ _____ _   _ 
@@ -49,21 +71,24 @@ class Mzitu:
             + 1: 日本妹子
             + 2: 台湾妹子
             + 3: 清纯妹子
+            + 4: 全部
             + Q: 退出
         """
         if not os.path.exists('data'):
             os.mkdir('data')
 
-        self.color()
-        print(self.blue, output, selectList)
+        self.color();
+        print(self.green, output, selectList)
+        
         try:
             select = input("请输入:")
-            if select.lower() == 'q':exit(self.green+"Info: 退出成功！")
-            if int(select) not in [0,1,2,3]:exit(self.red+ "Error: 输入错误！")
+            os.system("clear")
+            if select.lower() == 'q':exit("Info: 退出成功！")
+            if int(select) not in [0,1,2,3,4]:exit(self.red + "Error: 输入错误！")
             self.url = self.urls[int(select)]
+            self.dir = self.dirs[int(select)]
         except TypeError:
-            print(self.red, "Error: 输入错误！")
-
+            print("Error: 输入错误！")
 
     #控制台颜色
     def color(self):
@@ -71,100 +96,122 @@ class Mzitu:
         self.red   = "\033[0;31m"
         self.blue  = "\033[0;34m"
 
-    # 获取首页图集url
-    def getIndexUrl(self):
-        response = requests.get(url=self.url, headers=self.headers)
+
+
+    # 获取首页所有链接的url
+    def getIndexInfo(self, url):
+        response = requests.Session().get(url=url, headers=self.headers)
         urlList = re.compile("https:\/\/www.mzitu.com\/\d+").findall(response.text)
         # 去重
         new = []
         for id in urlList:
             if id not in new:new.append(id)
-        return new
+    
+        pageNum = re.compile("helli.*?<\/span>\\n.*?(\d+)<").findall(response.text)
+        return [int(pageNum[0]), new]
 
-    # 获取图片链接
-    def getImageUrl(self, html):
-        pattern = re.compile(
-            r'<div class="main-image"><p>.*?><img\ssrc="(.*?)".*<\/a>')
-        result = pattern.findall(html)
-        return result[0]
-
-    # 获取分页数
-    def getPageNum(self, html):
-        tmp = re.compile('<div class="pagenavi">([\S\s]*)下一页&raquo').findall(html)[0]
-        page = re.compile('<span>(.*?)<\/span>').findall(tmp)[-1]
-        return page
-
-    # 获取页面标题
-    def getPageTitle(self, url):
-        res = requests.get(url, headers=self.headers)
-        title = re.compile("<title>(.*?)<\/title>").findall(res.text)[0][:-13]
+    # 获取图集信息返回标题和分页数量
+    def getImagesInfo(self, url):
+        response = requests.Session().get(url=url, headers=self.headers)
+        title = re.compile("<title>(.*?)<").findall(response.text)[0]
         title = title.replace('-','_')
-        title = title.replace(' ','_')
-        return title
-
+        title = title.replace(' ','')
+        title = title.replace('_妹子图','')
+        title = title.replace('，','_')
+        title = title.replace('！','_')
+        
+        self.title = title
+  
+        page = 10
+        try:
+            page = re.compile('….*?<span>(\d+).*?下一').findall(response.text)[0]
+        except IndexError:
+            pass
+        return [title, int(page)]
+    
+    # 获取图片链接并保存
+    def getImage(self, url, index):
+        response = requests.Session().get(url=url, headers=self.headers)
+        imgUrl = re.compile('<.*?="blur" src="(.*?)"').findall(response.text)[0]
+        img = requests.Session().get(url=imgUrl, headers=self.headers) 
+        
+        fileName = str(index) + imgUrl[-4:]
+        
+        path = self.path + self.dir + self.pageNum +self.index + "_" + self.title  
+        
+        with open(path + '/' + fileName , 'wb') as f:
+            f.write(img.content)
+        f.close()
+    
     # 创建目录
-    def createDir(self, name):
+    def createDir(self, name, index):
+    
+        self.index = str(index)
+        
         name = name.strip()
         name = name.rstrip('\\')
-        path = 'data/'+ name
+        path = self.path + self.dir + self.pageNum + str(index) + "_" + name
+    
         isExists = os.path.exists(path)
         if not isExists:
-            os.mkdir(path)
-            print(self.green, "Info: 未下载，正在爬取！")
+            os.makedirs(path)
+            print(self.red + "Info: 未下载，正在爬取！")
             return True
         else:
-            print(self.green, "Info: 已存在，正在跳过！")
+            print(self.green + "Info: 已存在，正在跳过！")
             return False
 
-    # 请求分页图片
-    def requestPageImg(self, url, page):
-        # 请求分页
-        pageurl = url + "/" + str(page)
-        urllist = self.getImageUrl(requests.get(url=pageurl, headers=self.headers).text)
-        result = requests.get(url=urllist, headers=self.headers)
-        # 图片路径
-        path = 'data/' + self.title + str(datetime.now().microsecond) + '.' + urllist[-3:]
-        # 保存
-        with open(path, 'wb') as f:
-            f.write(result.content)
-        f.close()
-        print(self.green,"Info: 正在下载:" + path + " 进度：" + self.imageLen + "/" + str(page))
-        time.sleep(1)
 
-    # 请求首页url
-    def requestIndexUrl(self, urls, index):
-        self.url = urls[index]
-        response = requests.get(url=self.url, headers=self.headers)
-        html = response.text
-        self.imageLen = self.getPageNum(html)
-        print(self.green, "Info: 图片总数： " + self.imageLen)
-
-
-    # 主函数
+    # Main
     def main(self):
-        page = 1
-        urls = self.getIndexUrl()
-        for i in range(len(urls) + 1):
-            self.title = self.getPageTitle(urls[i]) + '/'
-            status = self.createDir(self.title)
-            if status == True:
-                self.title = self.getPageTitle(urls[i]) + '/'
-                self.requestIndexUrl(urls, i+4)
-            else: continue
-            for j in range(1, int(self.imageLen)):
-                if j == int(self.imageLen)-1:
-                    isGNext = input("Info:已爬取一组是否继续 y/n")
-                    if isGNext.lower() != 'y':exit()
-                self.requestPageImg(self.url, j)
 
-        isNext = input("Info: 已爬取整个页面是否继续? y/n")
-        if isNext.lower() == 'y':
-            page += 1
-            self.url = self.url + 'page/' +  str(page)
-            self.main()
-        else:exit(self.green+"Info: 退出成功！");
+        imgType = self.url
 
-if __name__ == "__main__":
-    mzitu = Mzitu()
-    mzitu.main()
-    # print(mzitu.getIndexUrl())
+        # 首页信息
+        indexInfo = self.getIndexInfo(imgType)
+        indexPage = indexInfo[0]
+
+
+        for i in range(1, indexPage+1):
+
+            # 获取图集链接
+            indexUrls = self.getIndexInfo(url=imgType + "page/" + str(i) +"/")
+
+            indexPo = Pool(5)
+            print("Info: 正在爬取第" + str(i))
+            for j in range(1, len(indexUrls[1])):
+                
+                self.pageNum = str(i) + "/"
+                
+                info = self.getImagesInfo(url=indexUrls[1][j])
+                print("Info: 正在爬取 - " + self.title + " 图片总数:" + str(info[1]))
+                isNext = self.createDir(info[0], j)
+                if isNext == False: continue
+                
+                po=Pool(5)
+                for k in tqdm(range(1, info[1] + 1)):
+                    po.apply_async(func=self.getImage,args=(indexUrls[1][j] + "/" + str(k), k,))
+                    time.sleep(0.4)
+                    # self.getImage(url=indexUrls[1][j] + "/" + str(k), index=k)
+                po.close()#关闭进程池，关闭后就不再接受新的请求，即开始执行任务。
+                print("Info: 正在等待线程结束...")
+                po.join()                     
+                
+                time.sleep(1)
+
+                while True:
+                    res = input(self.red + "Info: 以及爬取一组是否继续Y\\n: ")
+                    if res.lower() == 'y' or res.lower() == '' :
+                        break
+                    elif res.lower() == 'n':
+                        exit()
+                  
+               
+if __name__ == '__main__':
+    try:
+        mezi = Mzitu()       
+        mezi.main()
+    except KeyboardInterrupt:
+        print("\n退出成功")
+
+
